a09469e3bb0fd62b8ebc333d4f6c7e2aad326989
Alex Mariot Bender <alexbenderkv@gmail.com>
2019-09-09 15:42:52 -0300

    Initial Commit

---
+++ src/app/controllers/NotificationController.js
@@ -0,0 +1,34 @@
+import Notification from '../schemas/Notification';
+import User from '../models/user';
+
+class NotificationController {
+  async index(req, res) {
+    const checkIsProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+    if (!checkIsProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Only provider can list Notifications' });
+    }
+
+    const notification = await Notification.find({
+      user: req.userId,
+    })
+      .sort({ createdAt: 'desc' })
+      .limit(20);
+
+    return res.json(notification);
+  }
+
+  async update(req, res) {
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id,
+      { read: true },
+      { new: true }
+    );
+    return res.json(notification);
+  }
+}
+
+export default new NotificationController();
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://b6925080e3d042bb8b2467aa917212d0@sentry.io/1545935',
+};
